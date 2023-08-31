50a7ccd20c83a4b0da0a3c0891ee48f4ee3b8f18
Misael <maq3009@hotmail.com>
2023-08-31 04:50:14 -0400

    Added Boiler Label and image to Login

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-import kivy
-from kivy.uix.screenmanager import ScreenManager
-from kivymd.app import MDAPP
+
+from kivy.uix.screenmanager import ScreenManager, NoTransition
+from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
+import kivy
 
+kivy.require('2.2.1')
 
+Window.size = (540, 960)
 
-
-from kivy.app import App
-from kivy.uix.label import Label
-
-
-class MyApp(App):
-    
+class LogInScreen(MDApp):
     def build(self):
-        return Label(text = 'Hello World')
+        self.manager = ScreenManager(transition = NoTransition())
+        self.manager.add_widget(Builder.load_file("pre-splash.kv"))
+        return self.manager
+
+
+if __name__ == "__main__":
+    LogInScreen().run()
+
+
+# from kivy.app import App
+# from kivy.uix.label import Label
+
+
+# class MyApp(App):
     
-if __name__  == '__main__':
-    MyApp().run()
+#     def build(self):
+#         return Label(text = 'Hello World')
+    
+# if __name__  == '__main__':
+#     MyApp().run()
