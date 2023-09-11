13c55fc62c10c3fe7eeac4db00b3b0ef8cc47097
Misael <maq3009@hotmail.com>
2023-09-11 01:27:08 -0400

    third commit plus mainpy

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import kivy
 from kivy.uix.screenmanager import ScreenManager
-from kivymd.app import MDAPP
+from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
 
 
+Window.size = (360, 800)
 
-
-from kivy.app import App
-from kivy.uix.label import Label
-
-
-class MyApp(App):
-    
+class LogInApp(MDApp):
     def build(self):
-        return Label(text = 'Hello World')
-    
+        global screen_manager
+        screen_manager = ScreenManager()
+        #self.manager = ScreenManager(transition = NoTransition())
+        screen_manager.add_widget(Builder.load_file("pre-splash.kv"))
+        screen_manager.add_widget(Builder.load_file("login.kv"))
+        return self.manager
+        
+    def on_start(self):
+        Clock.schedule_once(self.login, 7)
+        
+    def login(self, *args):
+        self.manager.current = "login"
+
+
 if __name__  == '__main__':
-    MyApp().run()
+    LogInApp().run()
